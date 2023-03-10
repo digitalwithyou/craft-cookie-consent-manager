3bd85929cd00b93a158051ade8afe7026bba6ac7
Dieter Wyns <github@diewy.be>
2023-03-10 11:34:53 +0100

    Better cp navigation

--- src/plugin/CpSection.php
+++ src/plugin/CpSection.php
@@ -20,13 +20,13 @@ trait CpSection
 
         $item['label'] = $this->getSettings()->name;
 
-        // if (Craft::$app->getUser()->checkPermission('cookie-consent-manager-manageCategories')) {
-        //     $item['subnav']['categories'] = ['label' => 'Categories', 'url' => 'cookie-consent-manager/categories'];
-        // }
+        if (Craft::$app->getUser()->checkPermission('cookie-consent-manager-manageCategories')) {
+            $item['subnav']['categories'] = ['label' => 'Categories', 'url' => 'cookie-consent-manager/categories'];
+        }
 
-        // if (Craft::$app->getUser()->getIsAdmin() && Craft::$app->getConfig()->getGeneral()->allowAdminChanges) {
-        //     $item['subnav']['settings'] = ['label' => 'Settings', 'url' => 'cookie-consent-manager/settings'];
-        // }
+        if (Craft::$app->getUser()->getIsAdmin() && Craft::$app->getConfig()->getGeneral()->allowAdminChanges) {
+            $item['subnav']['settings'] = ['label' => 'Settings', 'url' => 'cookie-consent-manager/settings'];
+        }
 
         return $item;
     }
